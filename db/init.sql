6caa47b1d684ac8a252941206feb3e9bc91d646c
naomiamayaa <naomi.colombia.a@gmail.com>
2025-11-04 18:17:28 -0500

    Created db initiation and started main.py

---
+++ db/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    enrollment_date DATE
+);
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02')
+ON CONFLICT DO NOTHING;
